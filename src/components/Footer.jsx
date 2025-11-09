cbc7c07d916c9be949406f1cce0a22d5625755e3
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-11-09 12:09:54 -0500

    Fixed a footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ export default function Footer() {
     <footer>
       <a href="https://webpages.charlotte.edu/yrajput/itis3135">ITIS3135</a> ~{" "}
       <a href="https://github.com/yuvrajsr2" target="_blank" rel="noopener noreferrer">GitHub</a> ~{" "}
-      <a href="http://webpages.charlotte.edu/yrajput" target="_blank" rel="noopener noreferrer">CLT Web</a> ~{" "}
-      <a href="http://yuvrajsr2.github.io/" target="_blank" rel="noopener noreferrer">GitHub.io</a> ~{" "}
+      <a href="https://webpages.charlotte.edu/yrajput" target="_blank" rel="noopener noreferrer">CLT Web</a> ~{" "}
+      <a href="https://yuvrajsr2.github.io/" target="_blank" rel="noopener noreferrer">GitHub.io</a> ~{" "}
       <a href="https://www.linkedin.com/in/yrajput07/" target="_blank" rel="noopener noreferrer">LinkedIn</a> ~{" "}
       <a href="https://www.freecodecamp.org/yrajput" target="_blank" rel="noopener noreferrer">FreeCodeCamp</a>
 
       <p>
         Designed by{" "}
-        <a href="http://yuvrajsr2.github.io/yrajput.softwares.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://webpages.charlotte.edu/yrajput/itis3135/yrajput.softwares.com/" target="_blank" rel="noopener noreferrer">
           Yrajput Software Enterprises
         </a>{" "}
         @ 2025
